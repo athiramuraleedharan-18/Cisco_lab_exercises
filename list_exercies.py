3e96ea797a51af3e06311d893471c36f865f1f47
Athira Muraleedharan <atmuraleedharan18@gmail.com>
2025-01-27 19:28:27 +0530

    Update list_exercies.py

--- list_exercies.py
+++ list_exercies.py
@@ -20,3 +20,28 @@ print("The last element in the list: ", numbers[-1])
 
 #print the 2nd last element in the list
 print("The 2nd last element in the list: ",numbers[-2])
+
+
+#Adding elements to a list
+numbers=[111,7,2,1]
+print(len(numbers))
+print(numbers)
+
+
+##Append or add a number to the end of the list
+
+numbers.append(4)
+print(len(numbers))
+print(numbers)
+
+
+##Insert an element at the beginning of the list
+numbers.insert(0, 222)
+print(len(numbers))
+print(numbers)
+
+
+##Insert an element at index 1  of the list
+numbers.insert(1, 333)
+print(len(numbers))
+print(numbers)
