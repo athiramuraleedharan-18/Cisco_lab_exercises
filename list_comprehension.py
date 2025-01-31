d3552886130b55b3894c3060949035cdc83c9213
Athira Muraleedharan <atmuraleedharan18@gmail.com>
2025-01-31 15:44:06 +0530

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,12 @@
+#List comprehension
+#copy the entire list
+list_1 = [1]
+list_2 = list_1[:]#if no start and end mentioned by default entire list is considered
+list_1[0]=2
+print(list_2)
+
+
+#Copying some parts of list
+my_list=[10,8,6,4,2]
+new_list=my_list[1:3]
+print(new_list)
