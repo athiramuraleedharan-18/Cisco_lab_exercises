a87861d7beccb7d557589563b89dbaf3ec312fd9
Athira Muraleedharan <atmuraleedharan18@gmail.com>
2025-02-12 20:38:02 +0530

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -110,3 +110,10 @@ for number in bets:
 
 print(hits)
 
+#To find unique elements in list
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+temp_list=set(my_list)
+my_list=list(temp_list)
+print("The list with unique elements only:")
+print(my_list)
+
