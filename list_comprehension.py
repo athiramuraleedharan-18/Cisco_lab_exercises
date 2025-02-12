147c6ee13a81bada84cf68a8e8e5f654ecd8b694
Athira Muraleedharan <atmuraleedharan18@gmail.com>
2025-02-12 20:34:17 +0530

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -88,3 +88,25 @@ for i in my_list[1:]:
   if i > largest:
     largest = i
 print(largest)
+
+#Calculate hits in a draws in a bet.
+'''
+Let's assume that you've chosen the following numbers in the lottery: 3, 7, 11, 42, 34, 49.
+
+The numbers that have been drawn are: 5, 11, 9, 42, 3, 49.
+
+The question is: how many numbers have you hit?
+'''
+
+drawn=[5, 11, 9, 42, 3, 49]
+bets=[3, 7, 11, 42, 34, 49]
+hits=0
+
+#check for elements in bets that are in drawn
+
+for number in bets:
+  if number in drawn:
+    hits+=1
+
+print(hits)
+
