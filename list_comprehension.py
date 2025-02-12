da6629e2c94e9c2df4715fdea4aea0bb6503aa70
Athira Muraleedharan <atmuraleedharan18@gmail.com>
2025-02-12 14:27:16 +0530

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -17,3 +17,35 @@ new_list=my_list[1:-1]
 print(my_list)
 print(new_list)
 
+#list comprehension
+list_1=[1]
+list_2=list_1 #instead of contents memory reference is copied here
+list_1[0]=2
+print(list_2)
+#copying the entire list
+list_3=list_1[:] #slice operation , here only contents are copied as a brand new list
+list_1[0]=3
+print(list_3)
+
+#copying some parts of the list
+my_list=[10,8,6,4,2]
+new_list=my_list[1:3]
+print(new_list)
+
+#negative indices with slice.
+new_list=my_list[1:-1]
+print(new_list)
+
+#del instruction
+del my_list[1:3]
+print(my_list)
+
+#deleting contents all at once from a list
+del my_list[:]
+print(my_list)
+#here list becomes empty
+
+#by removing slice in above the whole list gets deleted.
+del my_list
+print(my_list)
+
