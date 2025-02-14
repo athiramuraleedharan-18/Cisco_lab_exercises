b9ba608c43354e1367f63a33902aa2b63183065f
Athira Muraleedharan <atmuraleedharan18@gmail.com>
2025-02-14 10:27:00 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,3 +12,11 @@ message()
 b=int(input())
 
 print(a,b,sep=",")
+
+def hello(name): #defining a function
+  print("Hello ",name) #Body of the function
+
+
+name=input("Enter your name:")
+
+hello(name) #calling/invoking the function
